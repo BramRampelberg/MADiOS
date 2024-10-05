8e61181d38fd42e5e30a438abf8a4e9f563d9c5e
Bram Rampelberg <bram.rampelberg@student.hogent.be>
2024-10-05 18:42:01 +0200

    Initial Commit

---
+++ MADiOSTests/MADiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  MADiOSTests.swift
+//  MADiOSTests
+//
+//  Created by Bram Rampelberg on 05/10/2024.
+//
+
+import Testing
+@testable import MADiOS
+
+struct MADiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
