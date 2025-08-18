fcaceb3233a2e2068939aee6e94051314aa73f41
Bram Rampelberg <bram.rampelberg@student.hogent.be>
2025-08-18 22:06:20 +0200

    fix data refetch bug when navigating

--- MADiOS/ui/pages/profile_page/ProfileViewModel.swift
+++ MADiOS/ui/pages/profile_page/ProfileViewModel.swift
@@ -16,7 +16,6 @@ class ProfileViewModel: ObservableObject {
     @MainActor
     init() {
         self.userModel = UserModel(email: "", firstName: "", familyName: "")
-        getUser()
     }
     
     var userState: UserState {
@@ -47,7 +46,7 @@ class ProfileViewModel: ObservableObject {
     }
     
     @MainActor
-    private func getUser() {
+    func getUser() {
         Task{
             let result = await userRepo.getUser()
             if (result.isSuccess){
